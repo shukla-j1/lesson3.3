1887605c658a533c9eba7af2fa7f933fb25868fd
Ubuntu <shuklaj1yahooco@shuklaj1yahooco.0ptmkub04jtufp1i25dt3bi2ya.gx.internal.cloudapp.net>
2020-01-09 21:35:05 +0000

    upper

--- src/main/java/com/boraji/tutorial/servlet/HelloWorldServlet.java
+++ src/main/java/com/boraji/tutorial/servlet/HelloWorldServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author imssbora
  */
 @WebServlet("/hello")
-public class HelloWorldServlet extends HttpServlet {
+public class HelloWorldsERVLEt extends HttpServlet {
 
    private static final long serialVersionUID = 1L;
 
